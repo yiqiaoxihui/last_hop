652bb752769aa1f841230433bd5af5501cdbeba4
yiqiaoxihui <937285794@qq.com>
2019-03-03 16:42:46 +0800

    add 10w result

---
+++ ip.10w.onlyerfen.r
@@ -0,0 +1,59 @@
+ip.10w.onlyerfen
+p_sum 43927
+3 3 0
+7 157 0
+8 373 0
+9 544 1
+10 1915 4
+11 2596 5
+12 2075 4
+13 2553 5
+14 3570 8
+15 9941 22
+16 6568 14
+17 5368 12
+18 2745 6
+19 1649 3
+20 1173 2
+21 884 2
+22 672 1
+23 441 1
+24 310 0
+25 169 0
+26 87 0
+27 52 0
+28 35 0
+29 31 0
+30 16 0
+action: 71436
+get last hop count: 43010 43010 60
+***************************icmp****************************
+receive upd port unreachable: 0 0
+--receive port unreachable,but last hop no reply: 0 0
+--receive port unreachable,and get last hop 0 0
+***************************guess****************************
+number need to guest: 1 0
+--first ping get,no get: 0 0
+guest ttl success: 43927 0 102
+guest ttl fail: 27509
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--division to guest ttl success: 0
+--no need to traceroute: 4152
+middle_not_reply: 23343
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+--one_step_success: 0
+----get_by_no_reply,all second send no reply: 0 0
+first predict ttl by ping too small: 0
+first predict ttl by ping too big: 0
+*************************send packet**************************
+method 1 all udp send packet: 71436 71436 0
+method 1 traceroute send packet: 0
+method2 all guest send packet: 356703
+222616
+method2 traceroute send packet: 675604
+--first ping 1
+--reset_ttl_by_traceroute 0
+--set_ttl_and_send: 356702
+----send_again 0
+my send packet: 428139
+traceroute: 675604
